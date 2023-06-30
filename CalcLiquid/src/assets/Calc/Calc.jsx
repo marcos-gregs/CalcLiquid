386ae40bd3fd1903c74d3ef81f1c958149834cfd
marcos-gregs <marcosgregs2019@gmail.com>
2023-06-30 11:50:59 -0300

    Newe 2

--- CalcLiquid/src/assets/Calc/Calc.jsx
+++ CalcLiquid/src/assets/Calc/Calc.jsx
@@ -1,23 +1,24 @@
-import { useEffect, useState } from "react"
+import {  useState } from "react"
 import './CalcStyle.css'
 export const Calc = () =>{
-    const [ResultDesc, setResultDesc] = useState(0)
+    const [ResultDesc , setResultDesc] = useState(0)
     const [ResultTotal,setResultTotal] = useState(0)
     const [ValueBruto ,setValueBruto] = useState(0)
     const [DescValue ,setDescValue] = useState(0)
     const [PerLiquid, setPerLiquid] = useState(0)
 
     const setAccount =() =>{
-        let account = (ValueBruto * DescValue)/100
-        let TotalAccount =  ValueBruto - account
-        let PerLiqui = (ValueBruto - TotalAccount)/100
+        let account =((ValueBruto * DescValue)/100)
+        let TotalAccount = parseInt(account) +  parseInt(ValueBruto)
+        let PerAccoutn = ((TotalAccount - ValueBruto)/ValueBruto)*100;
         setResultDesc(account)
         setResultTotal(TotalAccount)
-        setPerLiquid(PerLiqui)
+        setPerLiquid(PerAccoutn)
         setDescValue(0)
         setValueBruto(0)
 
     }
+
 
     const GetValueBruto = (eve) => {
         setValueBruto(eve.target.value)
@@ -31,20 +32,25 @@ export const Calc = () =>{
     <div className="MainDisplay">
 
         <div className="DisplayAction">
-            <label htmlFor="inputValueBruto">Valor Bruto:</label>
-            <input type="number" onChange={GetValueBruto} value={ValueBruto} />
-            <label htmlFor="inputValueLiqui">Valor Liquido:</label>
+            <div className="InputValue">
+                  <label htmlFor="inputValueBruto">Valor Liquido:</label>
+                    <input type="number" onChange={GetValueBruto} value={ValueBruto} />
+            </div>
+          <div className="InputValue">
+               <label htmlFor="inputValueLiqui">Porcetagem:</label>
             <input type="number" onChange={GetValueLiquid} value={DescValue} />
-            <button onClick={setAccount}>Account</button>
+           
+          </div>
+           <button onClick={setAccount}>Account</button>
         </div>
 
         <div className="DisplayResult">
             <div>
-                     <p>Desconto Liquido:</p>
+                     <p>Inclemento percentual:</p>
                      <span id="DescLiquid">{ResultDesc}</span>
             </div>
             <div>
-                  <p>Valor Liquido:</p>
+                  <p>Valor Bruto:</p>
                   <span id="ValueLiquid">{ResultTotal}</span>
             </div>
             <div>
